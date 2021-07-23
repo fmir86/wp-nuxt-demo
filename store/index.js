170a945223aecfbda3d482802dac55caad65c3fc
Fabian Miranda <fmir86@gmail.com>
2021-07-23 12:01:06 -0600

    first commit

--- store/index.js
+++ store/index.js
@@ -1,21 +1,46 @@
+/*
+this is where we will eventually hold the data for all of our posts
+*/
 export const state = () => ({
-    value: 'myvalue'
+    posts: []
 })
 
-export const getters = {
-    getterValue: state => {
-        return state.value
-    }
-}
-
+/*
+this will update the state with the posts
+*/
 export const mutations = {
-    updateValue: (state, payload) => {
-        state.value = payload
+    updatePosts: (state, posts) => {
+        state.posts = posts
     }
 }
 
+
+/*
+actions is where we will make an API call that gathers the posts,
+and then commits the mutation to update it
+*/
 export const actions = {
-    updateActionValue({ commit }) {
-        commit('updateValue', payload)
-    }
+    async getPosts({ state, commit }) {
+
+      if (state.posts.length) return;
+
+      try {
+        let posts = await fetch( `https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`).then( res => res.json() )
+        
+        posts = posts
+          .filter(el => el.status === "publish")
+          .map(({ id, slug, title, excerpt, date, tags, content }) => ({
+            id,
+            slug,
+            title,
+            excerpt,
+            date,
+            tags,
+            content
+          }))
+        commit("updatePosts", posts)
+      } catch (err) {
+        console.log(err)
+      }
+   }
 }
